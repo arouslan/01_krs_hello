0eb37ed9dfa094a6e9ff5347439986d5c3c15c28
arouslan <rouslan@inbox.ru>
2013-04-14 17:04:16 +0700

    Традиционно первый пример

---
+++ src/com/abushaev/task01/Hello.java
@@ -0,0 +1,17 @@
+package com.abushaev.task01;
+
+import java.util.Properties;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ruslan
+ * Date: 13.04.13
+ * Time: 23:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+
+    public static final void main(String[] arg){
+        System.out.println("Привет ми р!");
+    }
+}
